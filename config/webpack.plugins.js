62ff082ab2d2c9aedf5c98f74b262a87fcd1d29e
Raje Singh <raje.singh@dcmail.ca>
2021-12-01 18:43:27 -0500

    add copy-webpack-plugin

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -1,8 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
-  new BundleAnalyzerPlugin()
+  new BundleAnalyzerPlugin(),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, '../src', 'assets'),
+        to: path.resolve(__dirname, '../.webpack/renderer', 'assets')
+      }, 
+    ],
+    options: {
+      concurrency: 100,
+    },
+  }),
 ];
