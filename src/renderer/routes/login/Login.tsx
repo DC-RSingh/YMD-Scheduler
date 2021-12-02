9b134825919c370cd1db0ca661d31bbdb2c7110a
Raje Singh <raje.singh@dcmail.ca>
2021-12-02 07:53:38 -0500

    add login route

--- src/renderer/routes/login/Login.tsx
+++ src/renderer/routes/login/Login.tsx
@@ -1,10 +1,26 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
-import LoginContentContainer from "../../components/login-content/LoginContentContainer";
+import LoginDialogContainer from "../../components/login-dialog/LoginDialogContainer";
+import Image from '../../../assets/img/login-bg.jpg'; // Photo by Johannes Plenio from Pexels
+import makeStyles from "@mui/styles/makeStyles";
+
+const useStyles = makeStyles(() => ({
+    loginWrapper: {
+        display: 'flex',
+        height: '100vh', // have image take up 100% of viewport height
+        background: `linear-gradient(rgba(16, 16, 16, 0.8), rgba(16, 16, 16, 0.8)), url(${Image})`,
+        backgroundSize: 'cover',
+    },
+}));
 
 const Login: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <LoginContentContainer/>
+        <div className={classes.loginWrapper}>
+            <LoginDialogContainer/>
+        </div>
     );
 };
 
-export default Login;
+export default observer(Login);
