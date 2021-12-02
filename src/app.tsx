7b773f2782f495092d532f2e9d3a263f5ee951e6
Raje Singh <raje.singh@dcmail.ca>
2021-12-02 02:47:57 -0500

    change main div id from 'app' to 'root'

--- src/app.tsx
+++ src/app.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Login from "./renderer/routes/login/Login";
+import {StyledEngineProvider, ThemeProvider, CssBaseline, responsiveFontSizes} from "@mui/material";
+
+const theme = 2;
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/" render={() => <div>404</div>} />
-            </Switch>
-        </Router>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Switch>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/" render={() => <div>404</div>} />
+                    </Switch>
+                </Router>
+            </ThemeProvider>
+        </StyledEngineProvider>
     );
 };
 
 ReactDOM.render(
     <App />,
-    document.querySelector("#app")
+    document.querySelector("#root")
 );
