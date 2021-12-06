4d10317e57ccfcfbb1917724a7d54038558235a5
Raje Singh <raje.singh@dcmail.ca>
2021-12-06 14:28:31 -0500

    add react-table helper functions and interfaces

--- src/utils/helper.utils.ts
+++ src/utils/helper.utils.ts
@@ -1,2 +1,7 @@
 export const placeholderOption = 'None';
+
+export const hex2rgba = (hex: any, alpha = 1): string => {
+    const [r, g, b] = hex.match(/\w\w/g).map((x: any) => parseInt(x, 16));
+    return `rgba(${r},${g},${b},${alpha})`;
+};
   
